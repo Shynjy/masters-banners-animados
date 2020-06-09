b77af3aaaa7f7dd83313d4654ead35438a984a56
Shynjy <furia.designer@gmail.com>
2020-06-09 09:19:52 -0300

    git firt commit

---
+++ 300x250-animado/scripts.js
@@ -0,0 +1,94 @@
+init = function() {
+    setTimeout(function() {
+        allAnimations();
+    }, 200);
+}
+
+function allAnimations() {
+    var setTimer = 250;
+    var animCount = 0;
+    var animTimer = setInterval(function() { theTimer() }, setTimer),
+
+        fade = document.getElementById('fade'),
+
+        logo = document.getElementById('logo'),
+
+        bg1 = document.getElementById('bg1'),
+        bg2 = document.getElementById('bg2'),
+        bg3 = document.getElementById('bg3'),
+
+        copy1 = document.getElementById('copy1'),
+        copy2 = document.getElementById('copy2'),
+        copy3 = document.getElementById('copy3'),
+
+        // overlay = document.getElementById('overlay'),
+        // legal1 = document.getElementById('legal1'),
+
+        cta = document.getElementById('cta');
+
+    //  timings
+
+    function theTimer() {
+        let timer = 0;
+
+        function second(time) {
+            let x = Math.floor(time * 4)
+            timer = timer + x;
+            return timer
+        }
+
+        function contTimer(animCount) {
+            let total = animCount * setTimer - 1000
+            return `Time banner ${total / 1000}s`
+        }
+
+        if (animCount == second(0)) {
+            // Transition start
+            copy1.setAttribute('class', 'left-1');
+            copy2.setAttribute('class', 'left-1');
+            copy3.setAttribute('class', 'up-1');
+            // Transition end
+
+            bg1.setAttribute('class', 'fade-in');
+            bg1.setAttribute('class', 'transition-2 fade-in scale-1');
+            fade.setAttribute('class', 'transition-2 fade-out');
+        } else if (animCount == second(0.25)) {
+            copy1.setAttribute('class', 'transition-2 fade-in');
+            logo.setAttribute('class', 'transition-2 fade-in');
+        } else if (animCount == second(2.25)) {
+            copy1.setAttribute('class', 'transition-2 fade-out');
+        } else if (animCount == second(0.25)) {
+            bg2.setAttribute('class', 'transition-2 fade-in scale-1');
+            bg1.setAttribute('class', 'transition-off fade-off scale-1');
+            copy2.setAttribute('class', 'transition-2 fade-in');
+        } else if (animCount == second(2.25)) {
+            copy2.setAttribute('class', 'transition-2 fade-out');
+        } else if (animCount == second(0.25)) {
+            bg3.setAttribute('class', 'transition-2 fade-in scale-1');
+            bg2.setAttribute('class', 'transition-off fade-off scale-1');
+            copy3.setAttribute('class', 'transition-1 fade-in');
+            logo.setAttribute('class', 'transition-2 fade-out');
+        } else if (animCount == second(0.5)) {
+            cta.setAttribute('class', 'transition-2 fade-in');
+        } else if (animCount == second(0) + 4) {
+            clearInterval(animTimer);
+            console.log(contTimer(animCount))
+        }
+        animCount++;
+    }
+
+    //  legal stuff
+
+    //  legalOpen.addEventListener('mouseover', showLegal);
+    //  legalOpen.addEventListener('mouseleave', hideLegal);
+
+    //  function showLegal(){
+    // 	 legal1.setAttribute('class', 'transition-5 fade-in');
+    // 	 overlay.setAttribute('class', 'transition-5 fade-half');
+    //  }
+
+    //  function hideLegal(){
+    // 	 legal1.setAttribute('class', 'transition-5');
+    // 	 overlay.setAttribute('class', 'transition-5');
+    //  }
+}
---
+++ README.md
@@ -0,0 +1,16 @@
+# Masters para Banners Animados
+
+## 300x250 animado
+Banner animado:
+* 6s
+* fade de entrada
+* animações simples
+
+
+## 300x250 video
+Banner video:
+* 6s
+* fade de entrada
+* play automatico, após o termino do fade
+* btn replay
+* roda em todos os dispositivos 
